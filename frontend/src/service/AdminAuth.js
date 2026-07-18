3a3296fc2cc8929a4cab51467311d4f2b921a1ed
vishal rajbhar <vishalrajbharvr01@gmail.com>
2026-07-18 17:51:11 +0530

    AdminAuth.js

--- frontend/src/service/AdminAuth.js
+++ frontend/src/service/AdminAuth.js
@@ -5,12 +5,11 @@ export const loginadminAuth = async (data) => {
     return response;
 }
 
-
 export const changePassword = async (data) => {
     const token = localStorage.getItem("adminToken");
 
     const response = await axios.post(
-        "http://localhost:1000/admin/auth/api/admin/change-password",
+        "https://vishal-luxury-salon-booking.onrender.com/admin/auth/api/admin/change-password",
         data,
         {
             headers: {
@@ -23,6 +22,6 @@ export const changePassword = async (data) => {
 };
 
 export const getAllData = async () => {
-    const response = await axios.get("http://localhost:1000/admin/auth/api/find/Admin/data");
+    const response = await axios.get("https://vishal-luxury-salon-booking.onrender.com/admin/auth/api/find/Admin/data");
     return response;
 }
