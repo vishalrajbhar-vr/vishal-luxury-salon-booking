e85b99a1e5793a16220bb6da2657603ae9a68057
vishal rajbhar <vishalrajbharvr01@gmail.com>
2026-07-20 13:02:40 +0530

    new change

--- backend/src/utils/sendemail.js
+++ backend/src/utils/sendemail.js
@@ -1,14 +1,17 @@
 import nodemailer from "nodemailer";
 import dotenv from 'dotenv'
 dotenv.config({ path: './config.env' })
-const transporter = nodemailer.createTransport(
-    {
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587, // 587 is generally more reliable on cloud hosts
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS, // Gmail App Password
+  },
+});
 
 const sendEmail = async (to, subject, html) => {
     try {
